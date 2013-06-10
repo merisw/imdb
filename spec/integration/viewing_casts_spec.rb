4f8a9ae2e5f70782069949259de3bf30d036ed89
Meris Williams <merisw@gmail.com>
2013-06-10 00:32:45 -0700

    added readme

--- spec/integration/viewing_casts_spec.rb
+++ spec/integration/viewing_casts_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'Viewing Casts' do
   scenario "view a cast" do
     visit '/'
-    fill_in "Movie:", with: "Big"
+    fill_in 'title', with: "Big"
     click_button "View Cast"
     page.should have_content("Tom Hanks")
   end
